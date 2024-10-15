3e53e93a6463970e142d7d9b2e9fa29dc816c8fa
nucletic <kuntalpriyanshu608@gmail.com>
2024-10-15 16:07:51 +0530

    just finished writing the language agnostic code for the string calculator now moving to the front end part

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+// Language Agnostic Code
 
+function add(numbers) {
+  try {
+    if (numbers === "") return 0;
+    let regex = /^-?\d+(\.\d+)?$/
+    let digitRegex = /\d/
+    let digits = [];
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] === '-') {
+        if (numbers[i + 1] && digitRegex.test(numbers[i + 1])) {
+          throw new Error(`negative numbers not allowed ${numbers[i]}${numbers[i + 1]}`)
+        }
+      } else if (regex.test(numbers[i])) {
+        digits[digits.length] = numbers[i];
+      }
+    }
+    digits.forEach(digit => {
+      sum += parseInt(digit);
+    });
+    return sum;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
 
-
-
+add();
 
 
 
@@ -22,14 +46,15 @@
 
 
 // Language Specific Code
-let string = '1,2,3\nl5;6[[5=6'
-const regex = /(-?\d+)/g
 
-const stringCalculator = () => {
-  let sum = 0;
-  const digits = string.match(regex);
-  digits.forEach(num => {
-    sum += parseInt(num);
-  });
-  return sum;
-}
+// const stringCalculatorTwo = () => {
+//   let string = '1,2,3\nl5;6[[5=6'
+//   let regex = /(-?\d+)/g
+//   let sum = 0;
+
+//   const digits = string.match(regex);
+//   digits.forEach(num => {
+//     sum += parseInt(num);
+//   });
+//   return sum;
+// }
